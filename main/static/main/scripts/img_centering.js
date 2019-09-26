c02011b068376e32f40011d80ed8ee117b377a9e
RudyWhale <poetry.free@yandex.ru>
2019-09-27 02:07:20 +0300

    correct avatars display

--- main/static/main/scripts/img_centering.js
+++ main/static/main/scripts/img_centering.js
@@ -3,6 +3,16 @@ function onimageload(img){
   $(img).css('opacity', '1');
 }
 
+function onavatarload(img){
+  if ($(img).width() > $(img).height()){
+    $(img).css('left', function(){ return ($(this).parent().parent().width() - $(this).width()) / 2; });
+  }
+  else {
+    $(img).css('height', function(){ return ($(this).parent().parent().width() / $(this).width()) * $(this).height(); });
+    $(img).css('top', function(){ return ($(this).parent().parent().height() - $(this).height()) / 2; });
+  }
+}
+
 $(function(){
   $(window).resize(function(){
     $('img.center').css('left', function(){ return ($(this).parent().width() - $(this).width()) / 2; });

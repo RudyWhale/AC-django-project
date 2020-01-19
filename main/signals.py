2ea1a43c448bd2127ebe00cd4d1a575b9b5e828d
RudyWhale <poetry.free@yandex.ru>
2020-01-19 14:51:46 +0300

    Deleting old avatars when changed

--- main/signals.py
+++ main/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_delete, post_save, post_init
+from django.db.models.signals import post_delete, pre_save, post_save, post_init
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.core.signals import request_finished
@@ -10,9 +10,27 @@ import os
 # Deletes avatar file after profile deleting
 @receiver(post_delete, sender=ArtistProfile)
 def on_profile_delete(sender, instance, **kwargs):
+	# Taken from https://djangosnippets.org/snippets/10638/
 	if instance.avatar:
 		if os.path.isfile(instance.avatar.path):
 			os.remove(instance.avatar.path)
+
+
+@receiver(pre_save, sender=ArtistProfile)
+def delete_avatar_when_changed(sender, instance, **kwargs):
+	# Taken from https://djangosnippets.org/snippets/10638/
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = sender.objects.get(pk=instance.pk).avatar
+    except sender.DoesNotExist:
+        return False
+
+    new_file = instance.avatar
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
 
 
 # Deletes image file after instance deleting
